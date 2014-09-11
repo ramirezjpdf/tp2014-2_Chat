bc54e5816ecd3269fb0049085d5b5872aa3cef4a
ramirezjpdf <ramirez.jpdf@gmail.com>
2014-09-11 18:35:05 -0300

    send method for UDPbroker done. IOException in interface

--- ChatBroker/src/br/ufrj/tp/sockBroker/SockBroker.java
+++ ChatBroker/src/br/ufrj/tp/sockBroker/SockBroker.java
@@ -1,6 +1,8 @@
 package br.ufrj.tp.sockBroker;
 
+import java.io.IOException;
+
 public interface SockBroker {
-	public void send(String msg);
+	public void send(String msg) throws IOException;
 	public String recv();
 }
--- ChatBroker/src/br/ufrj/tp/sockBroker/UDPSockBroker.java
+++ ChatBroker/src/br/ufrj/tp/sockBroker/UDPSockBroker.java
@@ -1,16 +1,47 @@
 package br.ufrj.tp.sockBroker;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
 
 public class UDPSockBroker implements SockBroker{
-
-	public UDPSockBroker(DatagramPacket pkg){
+	private static int PORT = 1024;
+	private static int getPort(){
+		return PORT++;
+	}
+	
+	private class UDPClient{
+		private InetAddress adress;
+		private int port;
 		
+		public UDPClient(InetAddress adress, int port) {
+			this.adress = adress;
+			this.port = port;
+		}
+
+		public InetAddress getAdress() {
+			return adress;
+		}
+
+		public int getPort() {
+			return port;
+		}
+	}
+	
+	private UDPClient udpClient;
+	private DatagramSocket udpSocket;
+	
+	public UDPSockBroker(DatagramPacket pkg) throws SocketException{
+		this.udpClient = new UDPClient(pkg.getAddress(), pkg.getPort());
+		this.udpSocket = new DatagramSocket(getPort());
 	}
 	@Override
-	public void send(String msg) {
-		// TODO Auto-generated method stub
-		
+	public void send(String msg) throws IOException {
+		byte[] sendData = msg.getBytes();
+		DatagramPacket sendPkg = new DatagramPacket(sendData, sendData.length, udpClient.getAdress(), udpClient.getPort());
+		udpSocket.send(sendPkg);
 	}
 
 	@Override
