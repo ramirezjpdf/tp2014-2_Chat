da180fc159851d07b2ee31a23b0483dc330f6355
ramirezjpdf <ramirez.jpdf@ymail.com>
2014-09-12 01:55:30 -0300

    coded some test in run method

--- ChatBroker/src/br/ufrj/tp/broker/Broker.java
+++ ChatBroker/src/br/ufrj/tp/broker/Broker.java
@@ -1,6 +1,8 @@
 package br.ufrj.tp.broker;
 
 
+import java.io.IOException;
+
 import br.ufrj.tp.sockConnection.SockConnection;
 
 public class Broker implements Runnable{
@@ -14,7 +16,21 @@ public class Broker implements Runnable{
 
 	@Override
 	public void run() {
-		
+		System.out.println("Broker instantiated");
+		try {
+			sockConn.send("OK");
+			String msg = sockConn.recv();
+			while (!msg.trim().equals("END")){
+				sockConn.send(msg.toUpperCase());
+				msg = sockConn.recv();
+				System.out.println("Received from Client: " + msg + " => " + msg.equals("END"));
+				System.out.println("msg.equals(\"END\"): " + msg.trim().equals("END"));
+			}
+			System.out.println("Closing broker");
+		} catch (IOException e) {
+			System.out.println("ERROR - Could not send message from Borker to client");
+			e.printStackTrace();
+		}
 	}
 	
 }
