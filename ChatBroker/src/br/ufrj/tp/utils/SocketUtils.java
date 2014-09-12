1d52ae7ea92bee7af4b62aae616734f34f27d047
ramirezjpdf <ramirez.jpdf@ymail.com>
2014-09-12 00:17:25 -0300

    Add SocketUtils class with getAvaiblePort method

---
+++ ChatBroker/src/br/ufrj/tp/utils/SocketUtils.java
@@ -0,0 +1,16 @@
+package br.ufrj.tp.utils;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class SocketUtils {
+	public static int getAvailablePort() throws IOException{
+		ServerSocket sock = null;
+		try{
+			sock = new ServerSocket(0);
+			return sock.getLocalPort();
+		}finally{
+			sock.close();
+		}
+	}
+}
