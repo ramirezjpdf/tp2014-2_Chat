98a6c62f8e17801bc8ef0b2c5adeeabcbf8809be
ramirezjpdf <ramirez.jpdf@ymail.com>
2014-09-14 21:25:47 -0300

    add TCPSockListener

---
+++ ChatBroker/src/br/ufrj/tp/sockListener/TCPSockListener.java
@@ -0,0 +1,15 @@
+package br.ufrj.tp.sockListener;
+
+import java.io.IOException;
+
+import br.ufrj.tp.broker.BrokerFactory;
+
+public class TCPSockListener implements SockListener{
+
+	@Override
+	public void listen(BrokerFactory factory) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
