eb1e4fa77b11464f0299b1c5bae0c61f096c4edd
ramirezjpdf <ramirez.jpdf@ymail.com>
2014-09-14 21:10:26 -0300

    add SockConst class

--- ChatBroker/src/br/ufrj/tp/sockListener/UDPSockListener.java
+++ ChatBroker/src/br/ufrj/tp/sockListener/UDPSockListener.java
@@ -5,16 +5,14 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 
 import br.ufrj.tp.broker.BrokerFactory;
+import br.ufrj.tp.sockConnection.SockListenerConst;
 import br.ufrj.tp.sockConnection.UDPSockConnection;
 
-public class UDPSockListener implements SockListener{
-	private static final int port = 1023;
-	private static final int MSG_LEN = 1024;
-	
+public class UDPSockListener implements SockListener{	
 	@Override
 	public void listen(BrokerFactory factory) throws IOException {
-		DatagramSocket serverSocket = new DatagramSocket(port);
-		byte[] recvData = new byte[MSG_LEN];
+		DatagramSocket serverSocket = new DatagramSocket(SockListenerConst.PORT);
+		byte[] recvData = new byte[SockListenerConst.MSG_LEN];
 		
 		try{
 			while(true){
