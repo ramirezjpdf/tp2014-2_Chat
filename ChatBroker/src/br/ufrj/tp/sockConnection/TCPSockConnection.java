123cc5e8ef9f96d8c3bc04128136c47293a45859
ramirezjpdf <ramirez.jpdf@ymail.com>
2014-09-14 22:58:53 -0300

    implemented send and recv methods

--- ChatBroker/src/br/ufrj/tp/sockConnection/TCPSockConnection.java
+++ ChatBroker/src/br/ufrj/tp/sockConnection/TCPSockConnection.java
@@ -1,7 +1,10 @@
 package br.ufrj.tp.sockConnection;
 
 import java.io.IOException;
+
 import java.net.Socket;
+
+import br.ufrj.tp.sockListener.SockListenerConst;
 
 public class TCPSockConnection implements SockConnection {
 	private Socket clientSocket;
@@ -12,14 +15,16 @@ public class TCPSockConnection implements SockConnection {
 
 	@Override
 	public void send(String msg) throws IOException {
-		// TODO Auto-generated method stub
+		byte[] sendData = msg.getBytes();
+		clientSocket.getOutputStream().write(sendData);
 
 	}
 
 	@Override
 	public String recv() throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		byte[] recvData = new byte[SockListenerConst.MSG_LEN];
+		clientSocket.getInputStream().read(recvData);
+		return new String(recvData);
 	}
 
 }
