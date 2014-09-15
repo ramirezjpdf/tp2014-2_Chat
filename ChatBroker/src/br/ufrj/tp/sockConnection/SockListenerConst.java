c034df2868be260319575f634f2c432e3e635776
ramirezjpdf <ramirez.jpdf@ymail.com>
2014-09-14 21:10:44 -0300

    add SockConst class

---
+++ ChatBroker/src/br/ufrj/tp/sockConnection/SockListenerConst.java
@@ -0,0 +1,6 @@
+package br.ufrj.tp.sockConnection;
+
+public class SockListenerConst {
+	public static final int PORT     = 1023;
+	public static final int MSG_LEN  = 1024;
+}
---
+++ ChatBroker/src/br/ufrj/tp/sockListener/TCPSockListener.java
@@ -0,0 +1,5 @@
+package br.ufrj.tp.sockListener;
+
+public class TCPSockListener {
+
+}
