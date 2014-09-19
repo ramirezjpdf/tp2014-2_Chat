7dcf09266237ae6e33f511e84e9baa11bcfe92c0
Mateus Gregorio <mateusgreg.ufrj@gmail.com>
2014-09-19 19:18:03 -0300

    Implementacao da classe Server.

---
+++ ChatBroker/src/br/ufrj/tp/server/Server.java
@@ -0,0 +1,21 @@
+package br.ufrj.tp.server;
+
+import java.io.IOException;
+
+import br.ufrj.tp.broker.BrokerFactory;
+import br.ufrj.tp.sockListener.SockListener;
+
+public class Server {
+
+	private SockListener welcomeSocket;
+	private BrokerFactory brokerFactory; 
+	
+	public Server(SockListener listener) {
+		this.welcomeSocket = listener;
+		this.brokerFactory = new BrokerFactory();
+	}
+	
+	public void start() throws IOException {
+		welcomeSocket.listen(brokerFactory);
+	}
+}
