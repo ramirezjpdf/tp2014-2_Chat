67fdeef844c9dcaaa4fa6fcabcddb2997226dbd6
ramirezjpdf <ramirez.jpdf@ymail.com>
2014-09-20 02:30:35 -0300

    add username attribute with getter and constructor. no setter

--- ChatBroker/src/br/ufrj/tp/client/Client.java
+++ ChatBroker/src/br/ufrj/tp/client/Client.java
@@ -1,5 +1,13 @@
 package br.ufrj.tp.client;
 
 public class Client {
+	private String username;
 
+	public Client(String username) {
+		this.username = username;
+	}
+
+	public String getUsername() {
+		return username;
+	}
 }
