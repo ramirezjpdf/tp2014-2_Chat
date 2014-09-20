1c2911f77d6db54274f15ddfba21ce5431d711a9
ramirezjpdf <ramirez.jpdf@ymail.com>
2014-09-20 03:03:42 -0300

    add class ChatIdUtil

---
+++ ChatBroker/src/br/ufrj/tp/chat/ChatIdUtil.java
@@ -0,0 +1,17 @@
+package br.ufrj.tp.chat;
+
+import java.util.Collections;
+import java.util.List;
+
+import br.ufrj.tp.broker.Broker;
+
+public class ChatIdUtil {
+	public static String generateChatId(List<Broker> participants){
+		Collections.sort(participants);
+		String id = "";
+		for (Broker b : participants){
+			id += b.getClient().getUsername();
+		}
+		return id;
+	}
+}
