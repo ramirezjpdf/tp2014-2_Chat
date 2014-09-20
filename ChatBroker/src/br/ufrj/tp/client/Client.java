c572f41c9076104d2a65fbecb15d00a72c311f2c
ramirezjpdf <ramirez.jpdf@ymail.com>
2014-09-20 03:04:23 -0300

    add equals hash methods

--- ChatBroker/src/br/ufrj/tp/client/Client.java
+++ ChatBroker/src/br/ufrj/tp/client/Client.java
@@ -10,4 +10,30 @@ public class Client {
 	public String getUsername() {
 		return username;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
 }
