4ccdba75bb0813dd9d1ff04127ee7f2b5f45f803
Clauvin <clauvin_almeida@hotmail.com>
2014-09-20 22:02:56 -0300

    Contador de Brokers feitos em BrokerFactory

--- ChatBroker/src/br/ufrj/tp/broker/BrokerFactory.java
+++ ChatBroker/src/br/ufrj/tp/broker/BrokerFactory.java
@@ -3,7 +3,15 @@ package br.ufrj.tp.broker;
 import br.ufrj.tp.sockConnection.SockConnection;
 
 public class BrokerFactory {
+	
+	private int quantbrokersmade = 0;
+	
 	public Broker getBroker(SockConnection sockConn){
+		quantbrokersmade++;
 		return new Broker(sockConn);
+	}
+	
+	public int getBrokerquant(){
+		return quantbrokersmade;
 	}
 }
