2e4d3d71cecda4916f536a531dc09924d77f6d8f
Clauvin <clauvin_almeida@hotmail.com>
2014-09-21 16:07:33 -0300

    Com Bug - Nova Classe Server

--- ChatBroker/src/br/ufrj/tp/server/Server.java
+++ ChatBroker/src/br/ufrj/tp/server/Server.java
@@ -1,21 +1,58 @@
 package br.ufrj.tp.server;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import br.ufrj.tp.broker.BrokerFactory;
 import br.ufrj.tp.sockListener.SockListener;
 
-public class Server {
+public class Server implements Runnable{
 
-	private SockListener welcomeSocket;
+	
+	//TODO = Proteção decente de acesso aos arrays
+	//TODO = Tratar exceções
+	private int i = 0;
+	private ArrayList<Thread> threads;
+	private ArrayList<SockListener> welcomeSockets;
 	private BrokerFactory brokerFactory;
 	
 	public Server(SockListener listener) {
-		this.welcomeSocket = listener;
+		this.welcomeSockets.add(listener);
 		this.brokerFactory = new BrokerFactory();
+		this.threads = new ArrayList<Thread>();
+		threads.add(new Thread(this));
 	}
 	
-	public void start() throws IOException {
-		welcomeSocket.listen(brokerFactory);
+	public Server(ArrayList<SockListener> listener) {
+		this.welcomeSockets = listener;
+		this.brokerFactory = new BrokerFactory();
+		this.threads = new ArrayList<Thread>();
+		for(int k =0; k < welcomeSockets.size(); k++){
+			threads.add(new Thread(this));
+		}
+	}
+	
+	public void start() throws IOException{
+		for(int j = 0; j < threads.size(); j++){
+			threads.get(j).start();
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			i++;
+		}
+	}
+
+	@Override
+	public void run() {
+		
+		try {
+			welcomeSockets.get(i).listen(brokerFactory);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 }
--- ChatBroker/src/br/ufrj/tp/test/MainTestServer.java
+++ ChatBroker/src/br/ufrj/tp/test/MainTestServer.java
@@ -1,23 +1,27 @@
 package br.ufrj.tp.test;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import br.ufrj.tp.server.Server;
+import br.ufrj.tp.sockListener.SockListener;
 import br.ufrj.tp.sockListener.TCPSockListener;
 
 public class MainTestServer {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
-		TCPSockListener tcp = new TCPSockListener();
+		TCPSockListener tcp1 = new TCPSockListener();
+		TCPSockListener tcp2 = new TCPSockListener();
+		TCPSockListener tcp3 = new TCPSockListener();
+		TCPSockListener tcp4 = new TCPSockListener();
+		
+		ArrayList<SockListener> tcp = new ArrayList<SockListener>();
+		tcp.add(tcp1); tcp.add(tcp2);tcp.add(tcp3);tcp.add(tcp4);
+		
 		Server server = new Server(tcp);
 		
-		try {
-			server.start();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		server.start();
 		
 		
 		
