b70abe435ef08658e91467f344b97a50ad64d348
ramirezjpdf <ramirez.jpdf@ymail.com>
2014-09-25 00:59:40 -0300

    initiating console view

--- ChatBrokerClient/src/br/ufrj/tp/view/consoleView/MainConsoleView.java
+++ ChatBrokerClient/src/br/ufrj/tp/view/consoleView/MainConsoleView.java
@@ -1,5 +1,21 @@
 package br.ufrj.tp.view.consoleView;
 
-public class MainConsoleView {
+import java.util.Scanner;
 
+import br.ufrj.tp.controller.MainController;
+
+public class MainConsoleView {
+	private Scanner scan;
+	private MainController controller;
+	
+	public MainConsoleView(){
+		this.scan = new Scanner(System.in);
+		this.controller = new MainController();
+	}
+	
+	public void initiateConnection(){
+		System.out.println("Enter with your username: ");
+		String username = scan.nextLine();
+		
+	}
 }
