95e06dd284dbed8c73e86e42ed86b6fd46e34770
joao paulo de freitas ramirez <ramirezjpdf@leblon08.(none)>
2014-09-25 10:32:56 -0300

    main fone for handshake

--- ChatBrokerClient/src/br/ufrj/tp/main/Main.java
+++ ChatBrokerClient/src/br/ufrj/tp/main/Main.java
@@ -1,7 +1,13 @@
 package br.ufrj.tp.main;
 
+import br.ufrj.tp.utils.SocketUtils.TransportProtocol;
+import br.ufrj.tp.view.consoleView.MainConsoleView;
+
 public class Main {
 	public static void main(String[] args) {
-		//TODO
+		MainConsoleView mainView = new MainConsoleView();
+		mainView.initiateConnection(TransportProtocol.TCP);
+		System.out.println("Connection Successfull!!!");
+		System.exit(0);
 	}
 }
